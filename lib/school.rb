d1768fdd5024b6a345e32142b07e7b68847c4895
Thor Summerall <thor.summerall@gmail.com>
2020-12-01 03:57:58 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,8 +1,12 @@
 class School
   attr_accessor :name, :roster
+
   def initialize(name)
     @name = name
     @roster = {}
   end
 
+  def add_student(name, grade)
+    roster[grade] ||= []
+  end
 end
